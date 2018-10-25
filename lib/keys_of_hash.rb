67e69d67bf210dcf681862ebbb2eced16e5cccca
Adam Giegel <adam.giegel@gmail.com>
2018-10-25 17:42:56 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,16 @@
 class Hash
-  def keys_of(arguments)	 
-    # code goes here	   
-		animals = []
-		self.each { |animal, place|
-			if arguments.include?(place)
-				animals << animal
-			end
-		}
-		return animals
-  end	
-end 	
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
+    end
+    return array
+  end
+  
+end
